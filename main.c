0ad72d7363fb0da7e3b0e9ee74de15acb16c551e
Jaaf4r <jaafar.bouhnine@gmail.com>
2024-01-18 14:53:08 +0100

    monty

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - monty code interpreter
+* @ac: number of arguments
+* @av: monty file location
+* Return: 0 on success
+*/
+int main(int ac, char *av[])
+{
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	size_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int count = 0;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(av[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file <file>\n");
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		count++;
+		if (read_line > 0)
+		{
+			_execute(content, &stack, count, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+
+	return (0);
+}
